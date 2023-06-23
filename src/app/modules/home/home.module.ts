328f079e16fa5a18bce8736d76718c630b120cc2
Ricardo Carrillo <60947627+RicardoCarrillp@users.noreply.github.com>
2023-06-22 22:57:41 -0500

    footer fix y rsss , añadiendo models y data

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { NgxMarqueeModule } from 'ngx-marquee';
+import { SquaresModule } from 'src/app/shared/squares/squares.module';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule,
     NgxMarqueeModule,
+    SquaresModule
     ],
   declarations: [HomeComponent]
 })
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      `facebook`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../../../assets/img/facebook.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      `linkedin`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../../../assets/img/linkedin.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      `instagram`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../../../assets/img/instagram.svg")
+    );
+  }
 
   ngOnInit() {
   }
